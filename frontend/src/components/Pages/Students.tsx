887acf4f057650f2f51733cc166a9d261d42d122
Aryansinha2811 <sinhaaryan622@gmail.com>
2026-01-08 00:29:24 +0530

    All Done !

--- frontend/src/components/Pages/Students.tsx
+++ frontend/src/components/Pages/Students.tsx
@@ -1,10 +1,16 @@
 import Navbar from "../layout/Navbar"
+import StudentsHero from "../sections/StudentHero";
+import BenefitsSection from "../sections/StudentBenefit"
+import SuccessStories from "../sections/StudentTestimonial"
 import Footer from "../layout/Footer"
 
 const Students = () => {
     return (
             <div className="">
                 <Navbar />
+                <StudentsHero />
+                <BenefitsSection />
+                <SuccessStories />
                 <Footer />
             </div>
     )
